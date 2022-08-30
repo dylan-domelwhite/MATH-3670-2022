2591099addbaff56c7586fa1e019142691f3e3c3
dylan-domelwhite <112450142+dylan-domelwhite@users.noreply.github.com>
2022-08-29 21:47:22 -0500

    Added homework 1 MATLAB example

---
+++ Programming Examples/H1_KNearestNeighbors_MATLAB.m
@@ -0,0 +1,19 @@
+N = 1000;
+sigma = 1;
+
+x_train = 2*rand(N, 1);
+eps = sigma*randn(N, 1);
+y_train = 2*x_train + sin(2*pi*x_train) + eps;
+
+M = 100;
+x_test = linspace(0,2,M)';
+
+y_reg = 2*x_test + sin(2*pi*x_test);
+
+K=25;
+IDX = knnsearch(x_train, x_test, 'K', K);
+for m=1:M
+   y_predicted(m) = (1/K)*sum(y_train(IDX(m, :)));
+end
+
+plot(x_train, y_train, '.b', x_test, y_predicted, '-xr', x_test, y_reg, '-k')
